03005b4e58459c45f07872c03f4ef9727bd925d9
Byakki <lgn_19930923@163.com>
2020-07-01 17:15:48 +0800

    完善 demo -> index.html

--- bim-spring-boot-starter/src/test/java/com/github/byakkili/bim/demo/cmd/onlineuser/OnlineUsersHandler.java
+++ bim-spring-boot-starter/src/test/java/com/github/byakkili/bim/demo/cmd/onlineuser/OnlineUsersHandler.java
@@ -1,10 +1,11 @@
 package com.github.byakkili.bim.demo.cmd.onlineuser;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.github.byakkili.bim.core.BimSession;
 import com.github.byakkili.bim.core.protocol.impl.json.BaseJsonCmdHandler;
 import com.github.byakkili.bim.core.protocol.impl.json.JsonMsg;
-import com.github.byakkili.bim.demo.constant.GlobalConst;
 import com.github.byakkili.bim.demo.constant.Cmd;
+import com.github.byakkili.bim.demo.constant.GlobalConst;
 import com.github.byakkili.bim.demo.dto.SimpleMsg;
 import com.github.byakkili.bim.demo.dto.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +25,11 @@ public class OnlineUsersHandler extends BaseJsonCmdHandler<SimpleMsg> {
 
     @Override
     public JsonMsg handle(SimpleMsg reqMsg, BimSession session) {
+        String userId = session.getUserId();
         List<UserInfo> users = redisTemplate.opsForHash().entries(GlobalConst.REDIS_ONLINE_USERS).entrySet()
-                .stream().map(entry -> {
+                .stream()
+                .filter(e -> ObjectUtil.notEqual(e.getKey(), userId))
+                .map(entry -> {
                     UserInfo userInfo = new UserInfo();
                     userInfo.setId(entry.getKey().toString());
                     userInfo.setNickname(entry.getValue().toString());
