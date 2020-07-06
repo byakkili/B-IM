ee63d60c968696195bb26e214eceb09a2d64620a
Byakki <lgn_19930923@163.com>
2020-07-06 11:10:31 +0800

    更新 Shell 脚本

--- bin/updateVersion.sh
+++ bin/updateVersion.sh
@@ -6,19 +6,30 @@
 # 2. 替换README.md的版本号
 #------------------------------------------------
 
-if [ ! -n "$1" ]; then
-    echo "ERROR: 新版本不存在，请指定参数1"
+if [[ ! -n "$1" ]]; then
+    echo "ERROR: 新版本不存在, 请指定参数1"
     exit
 fi
 
-# 版本
-version=$1
-
 # 项目路径
 projectPath=$(dirname $(readlink -f "$0"))/..
+# 新版本
+newVersion="$1"
+# 旧版本
+oldVersion=`cat ${projectPath}/bin/version.txt`
+
+if [[ ! -n "${oldVersion}" ]]; then
+    echo "ERROR: 旧版本不存在, 请确认bin/version.txt中信息正确"
+    exit
+fi
 
 # 替换所有模块pom.xml中的版本
-mvn versions:set -DnewVersion=$version -f $projectPath/pom.xml
+mvn versions:set -DnewVersion=${newVersion} -f ${projectPath}/pom.xml
 
-# 替换其它地方的版本
-$projectPath/bin/replaceVersion.sh "$version"
+# 替换README.md中的版本
+sed -i "s/${oldVersion}/${newVersion}/g" ${projectPath}/README.md
+
+# 保留新版本
+echo "${newVersion}" > ${projectPath}/bin/version.txt
+
+echo "版本更新成功: ${oldVersion} -> ${newVersion}"
